5af9c0b93d48aef5076e5def6934cbad561fe9fb
Sachin_Dell <sachin.justsmiling@gmail.com>
2022-09-18 17:20:12 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ToDo from "./apps/to_do/ToDo";
+
+function App() {
+  return (
+    <>
+      <ToDo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/apps/to_do/ToDo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./components/Login";
+// import SplashScreen from "./components/SplashScreen";
+
+function ToDo() {
+  return (
+    <>
+      {/* <SplashScreen /> */}
+      <Login />
+    </>
+  );
+}
+
+export default ToDo;
